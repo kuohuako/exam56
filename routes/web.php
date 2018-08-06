22f87a28e8ec87375236710a83c49ff0f8f53ec3
Gavin Tseng <kuohuako@gmail.com>
2018-08-06 19:01:07 +0800

    瞭解路由的運作

--- routes/web.php
+++ routes/web.php
@@ -9,19 +9,11 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome')->with('name', 'gavin')->with('say', '嗨！');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
